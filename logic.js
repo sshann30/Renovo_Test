df4a9b95e1e186e8d036b77b4786491202d1ceaf
Steve Shannon <you@example.com>
2019-07-05 14:17:51 -0400

    basic multiplication done

---
+++ logic.js
@@ -0,0 +1,17 @@
+// if left to right
+
+function multiplyBy()
+{
+        num1 = document.getElementById("accountNumber").value;
+        num2 = document.getElementById("multiplyingNumber").value;
+        document.getElementById("result").innerHTML = num1 * num2;
+}
+
+// if right to left
+
+// function multiplyBy()
+// {
+//         num1 = document.getElementById("accountNumber").value;
+//         num2 = document.getElementById("multiplyingNumber").value;
+//         document.getElementById("result").innerHTML = num1 * num2;
+// }
