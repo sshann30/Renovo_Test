dd7ec0640490836474e070905f478d62a5d838a2
Steve Shannon <you@example.com>
2019-07-05 15:13:10 -0400

    better explainatation and begining JS

--- logic.js
+++ logic.js
@@ -1,4 +1,6 @@
-// if left to right
+// // if left to right
+// if(document.getElementById('selectid').value == "val1") {
+    
 
 function multiplyBy()
 {
@@ -6,12 +8,15 @@ function multiplyBy()
         num2 = document.getElementById("multiplyingNumber").value;
         document.getElementById("result").innerHTML = num1 * num2;
 }
-
-// if right to left
+// }
+// // if right to left
+// if(document.getElementById('selectid').value == "val2") {
+    
 
 // function multiplyBy()
 // {
 //         num1 = document.getElementById("accountNumber").value;
 //         num2 = document.getElementById("multiplyingNumber").value;
 //         document.getElementById("result").innerHTML = num1 * num2;
+// }
 // }
